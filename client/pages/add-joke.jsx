8b0a0cdfdf4e8980afe6db7293182c3f8462cb64
Kirk Hahn <kirkhahn11@yahoo.com>
2021-05-17 22:32:55 +0000

    Add joke form is functional and saving to the database

--- client/pages/add-joke.jsx
+++ client/pages/add-joke.jsx
@@ -6,30 +6,57 @@ export default class AddJoke extends React.Component {
     super(props);
     this.state = {
       joke: '',
+      title: '',
       isClicked: false,
       categories: [],
-      modalHidden: false
+      modalHidden: false,
+      textDisabled: true,
+      categoryId: ''
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeJoke = this.handleChangeJoke.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.addCategory = this.addCategory.bind(this);
     this.modalClass = this.modalClass.bind(this);
     this.modalAppear = this.modalAppear.bind(this);
     this.categoryList = this.categoryList.bind(this);
+    this.textBoxDisabled = this.textBoxDisabled.bind(this);
+    this.categorySelect = this.categorySelect.bind(this);
   }
 
   componentDidMount() {
-    fetch('/api/jokeApp')
+    fetch('/api/jokeApp/categories')
       .then(res => res.json())
-      .then(categories => this.setState({ categories }));
+      .then(categories => this.setState({ categories, joke: '', title: '' }));
   }
 
-  handleChange(event) {
+  handleChangeJoke(event) {
     this.setState({ joke: event.target.value });
+  }
+
+  handleChangeTitle(event) {
+    this.setState({ title: event.target.value });
   }
 
   handleSubmit(event) {
     event.preventDefault();
+    const newJoke = {
+      joke: this.state.joke,
+      title: this.state.title,
+      categoryId: this.state.categoryId
+    };
+    fetch('/api/jokeApp', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJoke)
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ joke: '', title: '' });
+      });
   }
 
   handleClick(event) {
@@ -38,7 +65,7 @@ export default class AddJoke extends React.Component {
 
   addCategory(category) {
     const categoryList = [...this.state.categories];
-    fetch('/api/jokeApp', {
+    fetch('/api/jokeApp/category', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -64,14 +91,41 @@ export default class AddJoke extends React.Component {
     this.setState({ modalHidden: !this.state.modalHidden });
   }
 
+  categorySelect(event) {
+    if (event.target.value === '') {
+      this.setState({ textDisabled: true });
+
+    } else {
+      this.setState({ textDisabled: false, categoryId: event.target.value });
+    }
+  }
+
+  textBoxDisabled() {
+    if (this.state.textDisabled) {
+      return (
+        <div className="mb-3">
+          <input disabled type="text" className="form-control" placeholder="Select A Category" aria-label="Title" aria-describedby="basic-addon1" onChange={this.handleChangeTitle}></input>
+          <textarea disabled className="form-control mt-1" id="exampleFormControlTextarea1" rows="3" onChange={this.handleChangeJoke} placeholder='Please Select a Category'></textarea>;
+        </div>
+      );
+    } else {
+      return (
+        <div className="mb-3">
+          <input type="text" className="form-control" placeholder="Add A Title" aria-label="Add A Title" aria-describedby="basic-addon1" onChange={this.handleChangeTitle} value={this.state.title}></input>
+          <textarea className="form-control mt-1" id="exampleFormControlTextarea1" rows="3" onChange={this.handleChangeJoke} placeholder='Maybe Try Being Funny This Time' value={this.state.joke}></textarea>;
+        </div>
+      );
+    }
+  }
+
   categoryList() {
     const listCategories = this.state.categories.map(categories =>
-      <option key={categories.categoryId}>
+      <option key={categories.categoryId} value={categories.categoryId}>
         {categories.name}
       </option>
     );
     return (
-      <select className="form-select" aria-label="Default select example" name="category">
+      <select className="form-select" aria-label="Default select example" name="category" onChange={this.categorySelect}>
         <option value="">Choose A Category Asshole</option>
         {listCategories}
       </select>
@@ -92,7 +146,7 @@ export default class AddJoke extends React.Component {
               <button className="btn btn-primary category-button" type="button" onClick={this.modalAppear}>Add A Category</button>
             </div>
             <div className="mb-3">
-              <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" onChange={this.handleChange} placeholder='Please Select a Category'></textarea>
+             {this.textBoxDisabled()}
             </div>
             <div className="col-12 center">
               <button onClick={this.handleSubmit} className="btn btn-primary" type="submit">Save Joke</button>
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 const express = require('express');
 const db = require('./db');
 // const ClientError = require('./client-error');
+// const jwt = require('jsonwebtoken');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 
@@ -11,7 +12,7 @@ app.use(staticMiddleware);
 
 app.use(express.json());
 
-app.get('/api/jokeApp', (req, res, next) => {
+app.get('/api/jokeApp/categories', (req, res, next) => {
   const sql = `
   select "name",
          "categoryId"
@@ -22,7 +23,29 @@ app.get('/api/jokeApp', (req, res, next) => {
     .catch(err => next(err));
 });
 
-app.post('/api/jokeApp', (req, res) => {
+app.post('/api/jokeApp/signIn', (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({
+      error: 'Email and Password are required fields'
+    });
+    return;
+  }
+  const sql = `
+  insert into "Users" ("email", "password")
+  values ($1, $2)
+  returning *
+  `;
+  const params = [email, password];
+  db.query(sql, params)
+    .then(results => {
+      const [user] = results.rows;
+      res.status(201).json(user);
+    })
+    .catch(err => next(err));
+});
+
+app.post('/api/jokeApp/category', (req, res) => {
   const { category } = req.body;
   if (!category) {
     res.status(400).json({
@@ -40,6 +63,35 @@ app.post('/api/jokeApp', (req, res) => {
     .then(results => {
       const [category] = results.rows;
       res.status(201).json(category);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'an unexpected error occured'
+      });
+    });
+});
+
+app.post('/api/jokeApp', (req, res) => {
+  const { joke, title, categoryId } = req.body;
+  const userId = 1;
+  const approxMinutes = 0;
+  if (!joke || !title) {
+    res.status(400).json({
+      error: 'Joke and Title are required fields'
+    });
+    return;
+  }
+  const sql = `
+  insert into "joke" ("categoryId", "userId", "title", "approxMinutes", "joke")
+  values ($1, $2, $3, $4, $5)
+  returning *
+  `;
+  const params = [categoryId, userId, title, approxMinutes, joke];
+  db.query(sql, params)
+    .then(results => {
+      const [newJoke] = results.rows;
+      res.status(201).json(newJoke);
     })
     .catch(err => {
       console.error(err);
